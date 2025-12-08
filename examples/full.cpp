60abfe7a2450814034a935c0ac7301247926ff46
Yuval Lifshitz <ylifshit@redhat.com>
2025-12-08 15:31:39 +0200

    add storage option API to connection builder
    fix double free issues with connection builder
    
    Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>

--- examples/full.cpp
+++ examples/full.cpp
@@ -106,7 +106,6 @@ int main() {
   }
   LanceDBConnection* db = lancedb_connect_builder_execute(builder);
   if (!db) {
-    lancedb_connect_builder_free(builder);
     std::cerr << "failed to connect to database" << std::endl;
     return 1;
   }
